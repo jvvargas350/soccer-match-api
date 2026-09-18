d06e8a974aa88133d4ea86e6255a16165ba09da8
Joe Vargas <joevv350@gmail.com>
2026-09-18 13:28:33 -0400

    Add initial API test with pytest

---
+++ tests/test_main.py
@@ -0,0 +1,24 @@
+from fastapi.testclient import TestClient
+
+from app.main import app
+
+
+client = TestClient(app)
+
+
+def test_home():
+    response = client.get("/")
+
+    assert response.status_code == 200
+    assert response.json() == {
+        "message": "Soccer Match API is running"
+    }
+
+
+def test_health():
+    response = client.get("/health")
+
+    assert response.status_code == 200
+    assert response.json() == {
+        "status": "ok"
+    }
